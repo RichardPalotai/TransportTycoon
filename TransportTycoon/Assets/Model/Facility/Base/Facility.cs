ba39881ad533accd5e630950423c9640ffdad7d5
Lajtaib0801 <jt8i26@inf.elte.hu>
2026-03-16 12:58:23 +0100

    Facility now implements all interfaces

--- TransportTycoon/Assets/Model/Facility/Base/Facility.cs
+++ TransportTycoon/Assets/Model/Facility/Base/Facility.cs
@@ -1,4 +1,29 @@
-public abstract class Facility
+public abstract class Facility : IBuildable, ITradeable, IUpdateable
 {
-    
+    public int Cost => throw new System.NotImplementedException();
+
+    public void Build(Map map, Tile tile)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Demolish()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Purchase(Player player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Sell()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Update(double deltaTime)
+    {
+        throw new System.NotImplementedException();
+    }
 }
