986549802cd00f038da2f2ad3b871f2e331d87c6
Lajtaib0801 <jt8i26@inf.elte.hu>
2026-03-19 22:22:06 +0100

    nullability added into Tile

--- TransportTycoon/Assets/Persistence/Map/Tile.cs
+++ TransportTycoon/Assets/Persistence/Map/Tile.cs
@@ -3,12 +3,14 @@ public sealed class Tile
     public bool IsFree { get; set; }
     public int X { get; init; }
     public int Y { get; init; }
-    public Facility Object;
-
-    public Tile(int x, int y, Facility @object)
+    #nullable enable
+    public Facility? Object;
+    #nullable disable
+    public Tile(int x, int y, Facility @object = null)
     {
         X = x;
         Y = y;
         Object = @object;
+        IsFree = Object is null;
     }
 }
