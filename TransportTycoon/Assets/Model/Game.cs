58057e5e85f5c9a0fcfa346bce075fa92d7bc000
Lajtaib0801 <jt8i26@inf.elte.hu>
2026-03-22 10:05:27 +0100

    Basic time management added

--- TransportTycoon/Assets/Model/Game.cs
+++ TransportTycoon/Assets/Model/Game.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
 
 public sealed class Game : IUpdateable
 {
@@ -7,7 +9,15 @@ public sealed class Game : IUpdateable
     private Player _player;
     public HashSet<Save> Saves { get; private set; }
     public DateTime CurrentTime { get; private set; }
-    public double TimeScale { get; set; } = 1.0;
+    private double _timeScale = 1.0;
+    private Stopwatch _stopwatch;
+
+
+    public double TimeScale
+    {
+        get { return _timeScale; }
+        set { _timeScale = Math.Max(0, value); }
+    }
     public bool IsPaused { get; private set; }
 
     public void NewGame()
@@ -15,11 +25,10 @@ public sealed class Game : IUpdateable
         CurrentTime = DateTime.Today;
         _map = new();
         _player = new();
+        _stopwatch = new();
+
     }
-    public void ResumeGame()
-    {
-        throw new System.NotImplementedException();
-    }
+    public void ResumeGame() => IsPaused = false;
     public void SaveGame()
     {
         throw new System.NotImplementedException();
@@ -32,21 +41,40 @@ public sealed class Game : IUpdateable
     {
         throw new System.NotImplementedException();
     }
-    public void UpdateGame()
+    public void UpdateGame(double deltaTime)
     {
-        throw new System.NotImplementedException();
+        if (IsPaused) return;
+
+        CurrentTime = CurrentTime.AddSeconds(deltaTime * TimeScale);
+
+
     }
     public void EndGame()
     {
         throw new System.NotImplementedException();
     }
-    public void PauseGame()
-    {
-        throw new System.NotImplementedException();
-    }
+    public void PauseGame() => IsPaused = true;
 
     public void Update(double deltaTime)
     {
         CurrentTime = CurrentTime.AddSeconds(deltaTime * TimeScale);
+    }
+
+    public void Loop()
+    {
+        _stopwatch.Start();
+
+        double lastTime = _stopwatch.Elapsed.TotalSeconds;
+
+        while (true)
+        {
+            double currentTime = _stopwatch.Elapsed.TotalSeconds;
+            double deltaTime = currentTime - lastTime;
+            lastTime = currentTime;
+
+            Update(deltaTime);
+
+            Thread.Sleep(16);
+        }
     }
 }
