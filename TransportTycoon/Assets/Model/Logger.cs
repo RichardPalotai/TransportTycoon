fae7ffe458e1e732182255c47b551fa7ddc356d2
Lajtaib0801 <jt8i26@inf.elte.hu>
2026-03-22 10:56:01 +0100

    Basic logger class added for time, prod and default messages

---
+++ TransportTycoon/Assets/Model/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public static class Logger
+{
+    public static void Log(string message)
+    {
+        Debug.Log($"[GAME] {message}");
+    }
+
+    public static void ProductionLog(Type facilityType, int amount)
+    {
+        Debug.Log($"[PROD] {facilityType.Name} facility produced:\t{amount}\t");
+    }
+
+    public static void LogTime(DateTime currentTime)
+    {
+        Debug.Log($"[TIME] {currentTime:dddd, yyyy MMMM dd HH:mm:ss}");
+    }
+}
