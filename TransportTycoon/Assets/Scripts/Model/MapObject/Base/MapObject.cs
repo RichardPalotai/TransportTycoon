6819cc060910b727dcb9df4b0d57a72f1bd10439
Lajtaib0801 <lajtaibenja@gmail.com>
2026-03-30 14:47:45 +0200

    MapObject id now increasing

--- TransportTycoon/Assets/Scripts/Model/MapObject/Base/MapObject.cs
+++ TransportTycoon/Assets/Scripts/Model/MapObject/Base/MapObject.cs
@@ -1,15 +1,20 @@
-﻿public class MapObject
+﻿public abstract class MapObject
 {
     private int _id = -1;
+    private static int _idCounter = 0;
     public int ID
     {
         get { return _id; }
-        set
+        init
         {
             if (_id == -1)
                 _id = value;
         }
     }
-    
+
+    protected MapObject()
+    {
+        ID = ++_idCounter;
+    }
 }
 
