bd10fc46eca07078b23e527715569c47825565b8
Lajtai Benjámin <jt8i26@inf.elte.hu>
2026-04-03 17:09:18 +0200

    Crossroad TrafficLight switching is implemented

--- TransportTycoon/Assets/Scripts/Model/GameEntity/Crossroad.cs
+++ TransportTycoon/Assets/Scripts/Model/GameEntity/Crossroad.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 public class Crossroad : IUpdateable
 {
@@ -6,10 +7,14 @@ public class Crossroad : IUpdateable
     public double GreenInterval { get; set; }
     private readonly double _yellowInterval = 2;
     private bool _northSouthGreen = true;
+    private double _timer;
+    private CurrentGreens _greens;
     public Crossroad()
     {
         TrafficLights = new();
         GreenInterval = 10;
+        _timer = 0;
+        _greens = CurrentGreens.NORTHSOUTH;
     }
     public void GreenLightIncrement()
     {
@@ -22,7 +27,65 @@ public class Crossroad : IUpdateable
     }
     public void Update(double deltaTime)
     {
-        //TODO
+        _timer += deltaTime;
+        if (_timer > GreenInterval + _yellowInterval)
+        {
+            if (_greens == CurrentGreens.NORTHSOUTH)
+                _greens = CurrentGreens.EASTWEST;
+            else
+                _greens = CurrentGreens.NORTHSOUTH;
+            _timer = 0;
+        }
+        ApplyLights();
+    }
+    /// <summary>
+    /// Sets the color of the lights in the crossroad
+    /// </summary>
+    // Simpler is better (maybe)
+    private void ApplyLights() 
+    {
+        if (_timer < GreenInterval) //Green state
+        {
+            foreach (var light in TrafficLights)
+            {
+                if (_greens == CurrentGreens.NORTHSOUTH)
+                {
+                    if (light.Direction == TrafficLight.LightDirection.NORTH ||
+                        light.Direction == TrafficLight.LightDirection.SOUTH)
+                    {
+                        light.Color = TrafficLight.LightColor.GREEN;
+                    }
+                    else
+                    {
+                        light.Color = TrafficLight.LightColor.RED;
+                    }
+                }
+                else
+                {
+                    if (light.Direction == TrafficLight.LightDirection.NORTH ||
+                        light.Direction == TrafficLight.LightDirection.SOUTH)
+                    {
+                        light.Color = TrafficLight.LightColor.RED;
+                    }
+                    else
+                    {
+                        light.Color = TrafficLight.LightColor.GREEN;
+                    }
+                }
+            }
+        }
+        else //Yellow state
+        {
+            foreach (var light in TrafficLights)
+            {
+                light.Color = TrafficLight.LightColor.YELLOW;
+            }
+        }
     }
 
+    private enum CurrentGreens
+    {
+        NORTHSOUTH,
+        EASTWEST
+    }
 }
