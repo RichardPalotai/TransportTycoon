67e28309ece6af32c02f248d4a8b746f8b4752c5
Ricsi <g4jd80@inf.elte.hu>
2026-04-16 18:59:15 +0200

    Added minimap UI and Code

--- TransportTycoon/Assets/Scripts/PlayerMovement/CameraController.cs
+++ TransportTycoon/Assets/Scripts/PlayerMovement/CameraController.cs
@@ -6,15 +6,24 @@ public class CameraController : MonoBehaviour
 {
     public static CameraController instance;
 
+    #region Public variables
     public Transform followTransform; // Transform reference for Vehicle object
     public Transform cameraTransform;
+    #endregion
 
-    public float normalSpeed;
-    public float fastSpeed;
-    public float moveSpeed;
-    public float moveTime;
-    public float rotationAmount;
-    public Vector3 zoomAmount;
+    #region Private variables
+    [SerializeField]
+    private float normalSpeed = 0.5f;
+    [SerializeField]
+    private float fastSpeed = 3f;
+    [SerializeField]
+    private float moveSpeed = 1f;
+    [SerializeField]
+    private float moveTime = 5f;
+    [SerializeField]
+    private float rotationAmount = 1f;
+    [SerializeField]
+    private Vector3 zoomAmount = new Vector3(0, -10, 10);
     
     public Vector3 newPosition;
     public Quaternion newRotation;
@@ -24,7 +33,9 @@ public class CameraController : MonoBehaviour
     public Vector3 dragCurrentPosition;
     public Vector3 rotateStartPosition;
     public Vector3 rotateCurrentPosition;
+    #endregion
 
+    #region Unity calls
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -51,9 +62,12 @@ public class CameraController : MonoBehaviour
         if (Keyboard.current != null && Keyboard.current.kKey.wasPressedThisFrame)
         {
             followTransform = null;
+            //GameViewModel.instance.DeselectObject();
         }
     }
+    #endregion
 
+    #region Private methods
     /// <summary>
     /// Handle mouse inputs and change the new position/rotation/zoom accordingly
     /// </summary>
@@ -179,4 +193,5 @@ public class CameraController : MonoBehaviour
             cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * moveTime); // between vectors
         }
     }
+    #endregion
 }
---
+++ TransportTycoon/Assets/Scripts/ViewModel/GameScreen/GameScreenHandlers/MinimapHandler.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class MinimapHandler : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas PlayerLocationIcon_cnv;
+
+    void Awake()
+    {
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AdjustPlayerPositionIcon();
+    }
+
+    private void AdjustPlayerPositionIcon()
+    {
+        Quaternion rotation = Quaternion.Euler(90, Camera.main.transform.rotation.eulerAngles.y, Camera.main.transform.rotation.eulerAngles.z); ;
+        PlayerLocationIcon_cnv.transform.LookAt(PlayerLocationIcon_cnv.transform.position + rotation * Vector3.forward, rotation * Vector3.up);
+    }
+}
