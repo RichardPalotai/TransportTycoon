f95b57cf68c30136bc088c72ed82724529ecb168
DxDxS01 <adacsi.balint@gmail.com>
2026-04-16 21:29:58 +0200

    Fixed building placement and added demolish building. Changed placeable object resource files

--- TransportTycoon/Assets/Scripts/3DObjects/GridObject.cs
+++ TransportTycoon/Assets/Scripts/3DObjects/GridObject.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public abstract class GridObject : MonoBehaviour
 {
+    private static int _globalCounter = 0;
+
     public Placeable data;
     public Vector2Int position;
+    private int _id;
+
+    public int ID
+    {
+        get
+        {
+            return _id;
+        }
+    }
 
     public abstract void OnObjectPlaced();
+
+    protected virtual void Awake()
+    {
+        _id = _globalCounter;
+        _globalCounter++;
+    }
 }
