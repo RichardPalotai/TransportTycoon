f1f7983db918b560835ae0f0b61ae8f22d4e590e
DxDxS01 <adacsi.balint@gmail.com>
2026-04-20 12:16:34 +0200

    Roadbehaviour

--- Assets/Scripts/3DObjects/Placeables/Roads/StraightRoadScript.cs
+++ Assets/Scripts/3DObjects/Placeables/Roads/StraightRoadScript.cs
@@ -1,3 +1,4 @@
+using Moq;
 using UnityEngine;
 
 public class StraightRoadScript : GridObject
@@ -45,35 +46,90 @@ public class StraightRoadScript : GridObject
         return false;
     }
 
-    private void ApplyVisuals(int score)
+    public void ApplyVisuals(int score)
     {
+        // 1. Reset everything to hidden first so meshes don't overlap!
         Straight.SetActive(false);
         Turn.SetActive(false);
         Tri.SetActive(false);
         Quad.SetActive(false);
 
-
         switch (score)
         {
-            case 1:
+
+            case 0: // No neighbors
+            case 1: // North neighbor only
+            case 4: // South neighbor only
                 Straight.SetActive(true);
                 transform.rotation = Quaternion.Euler(0, 0, 0);
                 break;
-            case 2:
+
+            case 2: // East neighbor only
+            case 8: // West neighbor only
                 Straight.SetActive(true);
                 transform.rotation = Quaternion.Euler(0, 90, 0);
                 break;
-            case 4:
+
+            case 5: // North (1) + South (4)
                 Straight.SetActive(true);
                 transform.rotation = Quaternion.Euler(0, 0, 0);
                 break;
-            case 8:
+
+            case 10: // East (2) + West (8)
                 Straight.SetActive(true);
                 transform.rotation = Quaternion.Euler(0, 90, 0);
                 break;
+
+            case 3: // North (1) + East (2)
+                Turn.SetActive(true);
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+
+            case 6: // East (2) + South (4)
+                Turn.SetActive(true);
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                break;
+
+            case 12: // South (4) + West (8)
+                Turn.SetActive(true);
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                break;
+
+            case 9: // North (1) + West (8)
+                Turn.SetActive(true);
+                transform.rotation = Quaternion.Euler(0, 270, 0); // or -90
+                break;
+
+
+            case 11: // North (1) + East (2) + West (8)
+                Tri.SetActive(true);
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+
+            case 7: // North (1) + East (2) + South (4)
+                Tri.SetActive(true);
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                break;
+
+            case 14: // East (2) + South (4) + West (8)
+                Tri.SetActive(true);
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                break;
+
+            case 13: // North (1) + South (4) + West (8)
+                Tri.SetActive(true);
+                transform.rotation = Quaternion.Euler(0, 270, 0);
+                break;
+
+
+            case 15: // All four directions
+                Quad.SetActive(true);
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+
             default:
-                Straight.SetActive(true);
-                transform.rotation = Quaternion.Euler(0, 0, 0);
+                // This is a fallback just in case math goes weird
+                Debug.LogWarning($"Road encountered unknown bitmask score: {score}");
                 break;
         }
     }
