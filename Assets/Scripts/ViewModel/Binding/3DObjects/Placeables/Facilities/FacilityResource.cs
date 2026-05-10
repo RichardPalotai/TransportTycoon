a67d45772b94fe16dd60b1e1e3063269457e8e93
DxDxS01 <adacsi.balint@gmail.com>
2026-05-11 00:26:02 +0200

    Added City

--- Assets/Scripts/ViewModel/Binding/3DObjects/Placeables/Facilities/FacilityResource.cs
+++ Assets/Scripts/ViewModel/Binding/3DObjects/Placeables/Facilities/FacilityResource.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Facility", menuName = "Placeable objects")]
 public class FacilityResource : Placeable
 {
-    public enum Facility { FARM, FACTORY, MINE, SAWMILL, TRAFFICLIGHT, BUSSTOP }
+    public enum Facility { FARM, FACTORY, MINE, SAWMILL, TRAFFICLIGHT, BUSSTOP, CITY }
 
     public Facility FacilityObj;
 }
